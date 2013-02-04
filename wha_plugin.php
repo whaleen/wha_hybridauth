8c16dc9df5c9c6555d67f7d47ec4d6b9a01a5368
Joshua Vaage <joshuavaage@gmail.com>
2013-02-04 09:15:33 -0800

    At least it has an idea for a name

---
+++ wha_plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+# $plugin['name'] = 'wha_hybridauth';
+
+$plugin['version'] = '0.1';
+$plugin['author'] = '';
+$plugin['author_uri'] = '';
+$plugin['description'] = 'Hybridauth plugin for Textpattern';
+$plugin['type'] = 0; 
+
+
+if (0) {
+?>
+# --- BEGIN PLUGIN HELP ---
+
+h1. Textile-formatted help goes here
+
+# --- END PLUGIN HELP ---
+<?php
+}
+
+# --- BEGIN PLUGIN CODE ---
+
+// Plugin code goes here.  No need to escape quotes.
+
+# --- END PLUGIN CODE ---
+
+?>
